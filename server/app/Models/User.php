90b10163a0da0fe22d029b331a9946fc5a85262c
Arjun Dholakiya <arjundholakiyav@gmail.com>
2026-07-14 23:42:13 +0530

    feat: completed setup for oauth using passports module

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\Contracts\OAuthenticatable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements OAuthenticatable
 {
-   use HasApiTokens, HasFactory;
+   use HasApiTokens, HasFactory, Notifiable;
 
    protected $fillable = [
       'name',
--- server/config/auth.php
+++ server/config/auth.php
@@ -3,8 +3,7 @@
 use App\Models\User;
 
 return [
-
-    /*
+   /*
     |--------------------------------------------------------------------------
     | Authentication Defaults
     |--------------------------------------------------------------------------
@@ -15,12 +14,12 @@ return [
     |
     */
 
-    'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
-        'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
-    ],
+   'defaults' => [
+      'guard' => env('AUTH_GUARD', 'web'),
+      'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
+   ],
 
-    /*
+   /*
     |--------------------------------------------------------------------------
     | Authentication Guards
     |--------------------------------------------------------------------------
@@ -37,14 +36,19 @@ return [
     |
     */
 
-    'guards' => [
-        'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
-        ],
-    ],
+   'guards' => [
+      'web' => [
+         'driver' => 'session',
+         'provider' => 'users',
+      ],
 
-    /*
+      'api' => [
+         'driver' => 'passport',
+         'provider' => 'users',
+      ],
+   ],
+
+   /*
     |--------------------------------------------------------------------------
     | User Providers
     |--------------------------------------------------------------------------
@@ -61,19 +65,19 @@ return [
     |
     */
 
-    'providers' => [
-        'users' => [
-            'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', User::class),
-        ],
+   'providers' => [
+      'users' => [
+         'driver' => 'eloquent',
+         'model' => env('AUTH_MODEL', User::class),
+      ],
 
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
-    ],
+      // 'users' => [
+      //     'driver' => 'database',
+      //     'table' => 'users',
+      // ],
+   ],
 
-    /*
+   /*
     |--------------------------------------------------------------------------
     | Resetting Passwords
     |--------------------------------------------------------------------------
@@ -92,16 +96,19 @@ return [
     |
     */
 
-    'passwords' => [
-        'users' => [
-            'provider' => 'users',
-            'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
-            'expire' => 60,
-            'throttle' => 60,
-        ],
-    ],
+   'passwords' => [
+      'users' => [
+         'provider' => 'users',
+         'table' => env(
+            'AUTH_PASSWORD_RESET_TOKEN_TABLE',
+            'password_reset_tokens',
+         ),
+         'expire' => 60,
+         'throttle' => 60,
+      ],
+   ],
 
-    /*
+   /*
     |--------------------------------------------------------------------------
     | Password Confirmation Timeout
     |--------------------------------------------------------------------------
@@ -112,6 +119,5 @@ return [
     |
     */
 
-    'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
-
+   'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
 ];
